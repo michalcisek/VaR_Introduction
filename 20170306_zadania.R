0130891ac9d84d232c8e9c9b8fd2cfce7bf53a6a
Michał Cisek <mc56283@sgh.waw.pl>
2017-03-06 15:42:15 +0100

    zadania na spotkanie

---
+++ 20170306_zadania.R
@@ -0,0 +1,18 @@
+#sprawdzenie dostepnych akcji (stockSymbols)
+
+#pobranie danych (getSymbols)
+
+#przykladowa wizualizacja - xts
+
+#przykladowa wizualizacja - quantmod (barChart, candleChart)
+
+#obrobka danych do potrzebnego formatu, sprawdzenie braków
+
+#wizualizacja ggplot
+
+#pobranie przykladowego portfela zlozonego z kilku akcji
+
+#policzenie zwrotów cen (Delt), średniego zwrotu dla instrumentu i odchylenia standardowego
+
+#przeliczenie macierzy korelacji (cor, pearson)
+
