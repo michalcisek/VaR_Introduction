ff410e0cf31e3f3672640e75c343bf708a1c64ee
Michał Cisek <mc56283@sgh.waw.pl>
2017-04-03 15:08:06 +0200

    to do

---
+++ 20170403_TO_DO.R
@@ -0,0 +1,20 @@
+# 1. Pobrac portfel dla WIG20 (najlepiej za okres ok. roku). Najlepiej zdefiniować w sprytny sposób żeby sprawdzał 
+# jaki jest aktualny skład tego indeksu. Wersja minimum - zdefiniować wektor zawierający aktualny skład spółego 
+# wchodzących w WIG20
+
+# 2. Policzyc zwroty z akcji. Hint: Funkcja Delt()
+
+# 3. Policzyc średnie zwroty dla kolejnych akcji.
+
+# 4. Policzyc odchylenia standardowe dla kolejnych akcji.
+
+# 5. Policzyc macierz korelacji
+
+# 6. Spiac cala powyzsza procedure do postaci funkcji. Jako argumenty podawac spolki dla ktorych powyzsze
+# rzeczy maja byc policzone i date, albo sama date i wtedy dla zdefiniowanego wewnatrz funkcji portfela. Im
+# wieksza elastycznosc tym lepiej
+
+# Fajnie jak wam uda sie uniknac korzystania z petli for a zamiast nich korzystac z rodziny funkcji apply.
+# Jak wam sie nie uda to trudno, poprawimy razem.
+
+# Powodzenia!
